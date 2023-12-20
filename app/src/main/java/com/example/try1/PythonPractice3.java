9ddce66239e1dc116cb13b19ffff3dd0dd224fa7
aisanakr <aisanakarim04@gmail.com>
2023-12-20 19:59:29 +0600

    Aisana new one

--- app/src/main/java/com/example/try1/PythonPractice3.java
+++ app/src/main/java/com/example/try1/PythonPractice3.java
@@ -1,5 +1,27 @@
 package com.example.try1;
 
-public class PythonPractice3 {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PythonPractice3 extends AppCompatActivity {
+    ImageView next;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.python_practical3);
+        next = findViewById(R.id.next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PythonPractice3.this,LessonFourOpen.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
